c3a61ca8f8d3fa66811a1e3bf7c49b700cd9be0a
up1 <somkiat.p@gmail.com>
2020-12-26 17:23:13 +0700

    Fix test

--- __tests__/api_test.js
+++ __tests__/api_test.js
@@ -2,12 +2,14 @@ const request = require("supertest");
 const app = require("../app");
 
 test("First API Test", (done) => {
-  request(app)
-    .get("/users")
-    .expect("Content-Type", /json/)
-    .expect(200)
-    .end(function (err, res) {
-      if (err) throw err;
-      done();
-    });
+  request(app).get("/users").expect("Content-Type", /json/).expect(
+    200,
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz",
+    },
+    done
+  );
 });
