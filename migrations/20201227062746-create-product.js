ffc67c64742ba1716771905e6dff07ec26ff278b
up1 <somkiat.p@gmail.com>
2020-12-27 13:29:48 +0700

    Demo db

---
+++ migrations/20201227062746-create-product.js
@@ -0,0 +1,30 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Products", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      price: {
+        type: Sequelize.DECIMAL,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("NOW()"),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("NOW()"),
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Products");
+  },
+};
